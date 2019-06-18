18d427a44e8d293f30d80a5c6a46345007d135bc
Dale Larsen <dale.larsen@pinnsg.com>
2019-06-18 17:46:31 -0400

    Chapter 14 - Deferreds and Promises

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,26 +1,36 @@
-(function(window) {
+(function (window) {
     'use strict';
 
     var App = window.App || {};
+    var Promise = window.Promise;
 
     function DataStore() {
         this.data = {};
     }
 
-    DataStore.prototype.add = function(key, val) {
-        this.data[key] = val
+    function promiseResolvedWith(value) {
+        var promise = new Promise(function (resolve, reject) {
+            resolve(value);
+        });
+        return promise;
     }
 
-    DataStore.prototype.get = function(key) {
-        return this.data[key];
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+        return promiseResolvedWith(null);
     }
 
-    DataStore.prototype.getAll = function() {
-        return this.data;
+    DataStore.prototype.get = function (key) {
+        return promiseResolvedWith(this.data[key]);
     }
 
-    DataStore.prototype.remove = function(key) {
+    DataStore.prototype.getAll = function () {
+        return promiseResolvedWith(this.data);
+    }
+
+    DataStore.prototype.remove = function (key) {
         delete this.data[key];
+        return promiseResolvedWith(null);
     }
 
     App.DataStore = DataStore;
