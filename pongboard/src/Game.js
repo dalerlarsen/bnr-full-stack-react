815f90272c8a2265f8511e2ab37d785b610e2532
Dale Larsen <dale.larsen@pinnsg.com>
2019-06-21 09:10:53 -0400

    Chapter 28 - The Component Lifecycle refactor Game playerListItems

--- pongboard/src/Game.js
+++ pongboard/src/Game.js
@@ -2,23 +2,17 @@ import React from 'react';
 import Player from './Player';
 
 const Game = (props) => {
-    let playerListItems;
-    if (props.updateScore) {
-        playerListItems = props.gameData.map(player => (
-            <Player
-                key={player.id}
-                player={player}
-                updateScore={() => props.updateScore(player.id)}
-            />
-        ));
-    } else {
-        playerListItems = props.gameData.map(player => (
-            <Player
-                key={player.id}
-                player={player}
-            />
-        ));
-    }
+    const playerListItems = props.gameData.map(player => (
+        <Player
+            key={player.id}
+            player={player}
+            updateScore={
+                props.updateScore
+                    ? () => props.updateScore(player.id)
+                    : false
+            }
+        />
+    ));
 
     return (
         <ul>
